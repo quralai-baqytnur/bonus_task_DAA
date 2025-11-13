123e7c8cd3edd1cd242deb98cd7f2d53b0f2534e
quralai-baqytnur <isskaktus@gmail.com>
2025-11-13 21:39:18 +0500

    Implement union-find data structure

---
+++ src/main/java/algorithms/UnionFind.java
@@ -0,0 +1,31 @@
+package algorithms;
+
+public class UnionFind {
+    private int[] parent, rank;
+
+    public UnionFind(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] < rank[rootY])
+                parent[rootX] = rootY;
+            else if (rank[rootX] > rank[rootY])
+                parent[rootY] = rootX;
+            else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
